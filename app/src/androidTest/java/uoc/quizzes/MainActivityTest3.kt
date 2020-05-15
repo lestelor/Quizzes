0466c6f69f56f842f209cd8bc1709f5b52e85cb8
lestelor <claudi.fauli@gmail.com>
2020-05-15 17:37:08 +0200

    First version

--- app/src/androidTest/java/uoc/quizzes/MainActivityTest3.kt
+++ app/src/androidTest/java/uoc/quizzes/MainActivityTest3.kt
@@ -7,61 +7,29 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import org.hamcrest.Description
 import org.hamcrest.Matcher
-import org.hamcrest.Matchers.*
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class MainActivityTest {
+class MainActivityTest3 {
 
     @Rule
     @JvmField
     var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun mainActivityTest() {
-
-            val appCompatButton = onView(
-            allOf(
-                withId(R.id.button_send), withText("Enviar"),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.linearLayout),
-                        childAtPosition(
-                            withClassName(`is`("android.widget.LinearLayout")),
-                            4
-                        )
-                    ),
-                    0
-                )
-            )
-        )
-
-        appCompatButton.perform(scrollTo(), click())
-
-        // Here place the Toast checker. It checks that if no radiobutton is selected, the corresponding toast is visible
-        onView(withText(R.string.SelectOption)).inRoot(
-            withDecorView(
-                not(
-                    `is`(
-                        mActivityTestRule.activity!!.window.decorView
-                    )
-                )
-            )
-        ).check(matches(isDisplayed()))
-
-
+    fun mainActivityTest3() {
         val appCompatRadioButton = onView(
             allOf(
                 withId(R.id.radioButton2), withText("Sagrada Família"),
@@ -79,7 +47,7 @@ class MainActivityTest {
         )
         appCompatRadioButton.perform(scrollTo(), click())
 
-        val appCompatButton2 = onView(
+        val appCompatButton = onView(
             allOf(
                 withId(R.id.button_send), withText("Enviar"),
                 childAtPosition(
@@ -94,13 +62,24 @@ class MainActivityTest {
                 )
             )
         )
-        appCompatButton2.perform(scrollTo(), click())
+        appCompatButton.perform(scrollTo(), click())
 
-        // Here place the Text View checker. It checks that the correct answer is selected
-        val textView = onView(withId(R.id.tv_2))
-        textView.check(matches(withText(R.string.Texto_resultado)))
+        val textView = onView(
+            allOf(
+                withId(R.id.tv_2), withText("¡¡¡CORRECTO!!!!"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("¡¡¡CORRECTO!!!!")))
 
-        val appCompatButton3 = onView(
+        val appCompatButton2 = onView(
             allOf(
                 withId(R.id.button_a2), withText("Siguiente"),
                 childAtPosition(
@@ -113,7 +92,7 @@ class MainActivityTest {
                 isDisplayed()
             )
         )
-        appCompatButton3.perform(click())
+        appCompatButton2.perform(click())
 
         val appCompatRadioButton2 = onView(
             allOf(
@@ -132,7 +111,7 @@ class MainActivityTest {
         )
         appCompatRadioButton2.perform(scrollTo(), click())
 
-        val appCompatButton4 = onView(
+        val appCompatButton3 = onView(
             allOf(
                 withId(R.id.button_send), withText("Enviar"),
                 childAtPosition(
@@ -147,9 +126,9 @@ class MainActivityTest {
                 )
             )
         )
-        appCompatButton4.perform(scrollTo(), click())
+        appCompatButton3.perform(scrollTo(), click())
 
-        val appCompatButton5 = onView(
+        val appCompatButton4 = onView(
             allOf(
                 withId(R.id.button_a2), withText("Siguiente"),
                 childAtPosition(
@@ -162,7 +141,7 @@ class MainActivityTest {
                 isDisplayed()
             )
         )
-        appCompatButton5.perform(click())
+        appCompatButton4.perform(click())
 
         val appCompatRadioButton3 = onView(
             allOf(
@@ -181,7 +160,7 @@ class MainActivityTest {
         )
         appCompatRadioButton3.perform(scrollTo(), click())
 
-        val appCompatButton6 = onView(
+        val appCompatButton5 = onView(
             allOf(
                 withId(R.id.button_send), withText("Enviar"),
                 childAtPosition(
@@ -196,11 +175,22 @@ class MainActivityTest {
                 )
             )
         )
-        appCompatButton6.perform(scrollTo(), click())
+        appCompatButton5.perform(scrollTo(), click())
 
-        // Here place the Text View checker. It checks that all the correct answer are selected
-        val textView2 = onView(withId(R.id.tv_2))
-        textView2.check(matches(withText(R.string.Felicidades_txt)))
+        val textView2 = onView(
+            allOf(
+                withId(R.id.tv_2), withText("¡¡¡FELICIDADES!!! \n ¡¡¡HAS FINALIZADO LA LECCIÓN!!"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textView2.check(matches(withText("¡¡¡FELICIDADES!!!   ¡¡¡HAS FINALIZADO LA LECCIÓN!!")))
     }
 
     private fun childAtPosition(
@@ -220,6 +210,4 @@ class MainActivityTest {
             }
         }
     }
-
-
 }
